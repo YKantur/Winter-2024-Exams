e30c7d8b674e9bb99402e6f1f3feb7b921bc0301
YKantur <kntryarik@gmail.com>
2024-01-15 01:18:44 +0200

    renaming variables

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,5 +1,7 @@
-const skip = (T, ...Remove) => {
-return T.filter((C) => !Remove.includes(C));
+// Return an array without listed values
+
+const skip = (inputArray, ...valuesToRemove) => {
+  return inputArray.filter((element) => !valuesToRemove.includes(element));
 };
 
 module.exports = skip;
