b2a89403772295cfc149aaaea5480472659a3d2b
YKantur <kntryarik@gmail.com>
2024-01-15 01:17:03 +0200

    Refactor 'skip' function for сlarity

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,16 +1,5 @@
-// Return an remove without listed values
-
-Skip = (T, ...Remove) => {
-  x = 0;
-  for (C of T) {
-    for (X of Remove) {
-      if (C === X) {
-        T.splice(x, 1);
-      }
-    }
-    x++;
-  }
-  return T;
+const skip = (T, ...Remove) => {
+return T.filter((C) => !Remove.includes(C));
 };
 
-module.exports = Skip;
+module.exports = skip;
