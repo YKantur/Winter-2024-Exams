08c9ebb174d371cb4a2cd5fde79178b5ac719477
YKantur <kntryarik@gmail.com>
2024-01-15 01:54:44 +0200

    simplificated return

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -3,8 +3,7 @@
 // Shuffle an array
 
 const shuffleArray = (arr) => {
-  arr.sort(() => Math.random() - 0.5);
-  return arr;
+  return arr.sort(() => Math.random() - 0.5);
 };
 
 module.exports = shuffleArray;
