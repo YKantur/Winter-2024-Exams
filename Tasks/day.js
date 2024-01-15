eb9c8c14632bf7df5f4de9eedf2914a9d4b59649
YKantur <kntryarik@gmail.com>
2024-01-15 03:19:59 +0200

    improved if statement and removed variable

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,9 +5,8 @@
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
-  let i;
-  for (i = 0; i < DAYS.length; i++) {
-    if (s.toLowerCase().startsWith(DAYS[i].toLowerCase())) {
+  for (let i = 0; i < DAYS.length; i++) {
+    if (s.startsWith(DAYS[i].toLowerCase())) {
       return i + 1;
     }
   }
