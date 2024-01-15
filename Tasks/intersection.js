f491fee5e2d64a4cf9fb7687d9de63aaa7fcca5d
YKantur <kntryarik@gmail.com>
2024-01-15 02:59:01 +0200

    deleted useless code and renamed function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,15 +1,13 @@
 // Find an intersection of two dictionaries
 
-const intersection = (object_1, object_2) => {
+const findIntersection = (object_1, object_2) => {
   const firstKeys = Object.keys(object_1);
   for (const attributeName of firstKeys) {
-    if (object_1[attributeName] === object_2[attributeName]) {
-      object_2[attributeName] = object_1[attributeName];
-    } else {
+    if (object_1[attributeName] !== object_2[attributeName]) {
       delete object_1[attributeName];
     }
   }
   return object_1;
 };
 
-module.exports = intersection;
+module.exports = findIntersection;
