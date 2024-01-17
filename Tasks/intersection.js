51d70432994edde775a8b114f1775cafcf9fd008
YKantur <kntryarik@gmail.com>
2024-01-18 01:11:30 +0200

    fixed

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,13 +3,14 @@
 // Find an intersection of two dictionaries
 
 const findIntersection = (object1, object2) => {
-  const firstKeys = Object.keys(object1);
+  let copyObject1 = object1;
+  const firstKeys = Object.keys(copyObject1);
   for (const attributeName of firstKeys) {
-    if (object1[attributeName] !== object2[attributeName]) {
-      delete object1[attributeName];
+    if (copyObject1[attributeName] !== object2[attributeName]) {
+      delete copyObject1[attributeName];
     }
   }
-  return object1;
+  return copyObject1;
 };
 
 module.exports = findIntersection;
